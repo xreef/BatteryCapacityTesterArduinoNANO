c221a982d3833ed11b08c489621b5adadc3c69fe
Renzo <renzo.mischianti@marketwall.com>
2017-10-27 21:43:59 +0200

    Version0.2 with prefored board

--- BatteryCapacityTesterArduinoNANO.cpp
+++ BatteryCapacityTesterArduinoNANO.cpp
@@ -11,71 +11,95 @@
 #define MOSFET_PIN 5
 #define BAT_PIN A0
 #define RES_PIN A1
-#define BATTERY_THERM_PIN A2
-#define RESISTA_THERM_PIN A3
+#define BATTERY_THERM_PIN A3
+#define RESISTA_THERM_PIN A2
 #define BUZZER_PIN 9
 
 #define BATTERY_MAX_TEMP 50
 #define RESISTANCE_MAX_TEMP 70
 
-float batResValueGnd = 10.0;
-float batResValueVolt = 10.0;
+// Battery voltage resistance
+#define BAT_RES_VALUE_GND 10.0
+#define BAT_RES_VALUE_VCC 10.0
+// Power resistor voltage resistance
+#define RES_RES_VALUE_GND 10.0
+#define RES_RES_VALUE_VCC 10.0
 
-float resResValueGnd = 10.0;
-float resResValueVolt = 10.0;
+#define NO_BATTERY_VOLTAGE 1
+
+#define USING_BATTERY_TERMISTOR true
+#define USING_RESISTO_TERMISTOR true
+
+#define NUMBER_OF_LOW_TO_RAISE  5
+#define WAIT_AFTER_LOW 4000
+
+uint8_t lowRaised = 0;
 
 float capacity = 0.0; 	// capacity in mAh
-float resValue = 10.2;  // Resistor Value in Ohm
+float resValue = 10.0;  // Resistor Value in Ohm
 float vcc = 5.04; 		// Voltage of Arduino 5V pin ( overwritten by function )
-float tolerance = 0.2; 	// When raise first time the low voltage at next check I apply this tolerance to not restart if It's raise the value
+float minVoltageDropoutTolerance = 0.1; // If min value declared is 2.9 i apply 2.8 to compensate voltage oscillation
+//float tolerance = 0.2; 	// When raise first time the low voltage at next check I apply this tolerance to not restart if It's raise the value
 float current = 0.0; 	// current in Amp
 float mA = 0;         	// current in mA
 float batVolt = 0.0;  	// Battery Voltage
-float realBatVolt = 0.0;
+float realBatVolt = 0.0;// Battery voltage + resistance voltage
 float resVolt = 0.0;  	// Voltage at lower end of the Resistor
 
+// Valiable to manage some behaivor
+int prevStatus = 0; 				// Last state
+bool dischargingStarted = false;	// Fist time starting discharging
+float startingVcc = 4.2; 			// Voltage at the start of discharging (overwritten at first time)
+
+// Structure of battery type
 struct BatteryType {
-	char name[8];
+	char name[10];
 	float maxVolt;
 	float minVolt;
 };
 
-#define BATTERY_TYPE_NUMBER 3
+#define BATTERY_TYPE_NUMBER 4
 BatteryType batteryTipes[BATTERY_TYPE_NUMBER] = {
 		{ "18650", 4.3, 2.9 },
 		{ "17550", 4.3, 2.9 },
-		{ "14500", 4.3, 2.75 }
+		{ "14500", 4.3, 2.75 },
+		{ "6v Acid", 7.2, 5.8  }
 };
 
-// This parameter for 18650 battery type
+// Variable to use in discharging time
 float batHigh = 4.3; 	// Battery High Voltage
 float batLow = 2.9; 	// Discharge Cut Off Voltage
 
+// Value of button
+// button of change battery
 uint8_t buttonChangeState = LOW;
+// button of select battery
 uint8_t buttonSelectState = LOW;
 
+// Temporary selected battery
 uint8_t tmpBatterySelected = 0;
+// Selected battery
 int8_t batterySelected = -1;
-
+// First time must refresh display
 bool displayRefresh = false;
 
 unsigned long previousMillis = 0; 		// Previous time in ms
-unsigned long millisPassed = 0;  		// current time in ms
+unsigned long millisPassed = 0;  		// Using this variable to update screen more than update values (for animated effect)
 unsigned long waitMillisPassed = 3000;  // current time in ms
-float sample1 = 0;
-float sample2 = 0;
 int x = 0;
 int row = 0;
 
-int loadLenght = 3;
-
+// Variable to hold mosfet status
 int mosfetStatus = LOW;
 
-LiquidCrystal_I2C lcd(0x38, 16, 2);
-
+// Inizialize thermistor
 Thermistor thermBattery(BATTERY_THERM_PIN);
 Thermistor thermResistance(RESISTA_THERM_PIN);
 
+// Inizialize display
+LiquidCrystal_I2C lcd(0x38, 16, 2);
+// Additional display character
+int loadLenght = 3;
 byte loader[][8] = { { // 0 loader
 		B00001,
 		B00011,
@@ -104,28 +128,28 @@ byte loader[][8] = { { // 0 loader
 		B10101,
 		B10101,
 		B10011,
-		B11111 }, { // down load+1
+		B11111 }, { // arrow down load+1
 		B01110,
 		B01110,
 		B01110,
 		B01110,
 		B11111,
 		B01110,
-		B00100 }, { // up load+2
+		B00100 }, { // arrow up load+2
 		B00100,
 		B01110,
 		B11111,
 		B01110,
 		B01110,
 		B01110,
-		B01110 }, {  // up load+3
+		B01110 }, {  // ° load+3
 		B00110,
 		B01001,
 		B00110,
 		B00000,
 		B00000,
 		B00000,
-		B00000, }, {  // up load+4
+		B00000, }, {  // thermometer load+4
 		B00100,
 		B01010,
 		B01010,
@@ -133,10 +157,6 @@ byte loader[][8] = { { // 0 loader
 		B11011,
 		B10001,
 		B01110, } };
-
-int prevStatus = 0;
-bool dischargingStarted = false;
-float startingVcc = 4.2; // Voltage at the start of discharging (overwritten at first time)
 
 uint8_t lastTempVisualizedBattery = 0;
 int loadingCursor = 0;
@@ -157,15 +177,13 @@ void setup() {
 	mosfetStatus = LOW;  			// MOSFET status is off during the start
 	Serial.println("CLEARDATA");
 
+	// Inizialize LCD
 	lcd.init();
-
 	lcd.clear();
-
 	int i;
 	for (i = 0; i < loadLenght + 5; i++) {
 		lcd.createChar(i, loader[i]);
 	}
-
 	lcd.setBacklight(LOW);
 
 	pinMode(SELECT_BATTERY_BUTTON, INPUT);
@@ -177,6 +195,9 @@ void loop() {
 	vcc = readVcc() / 1000.0;
 
 	//************ Measuring Battery Voltage ***********
+	float sample1 = 0;
+	float sample2 = 0;
+
 	for (int i = 0; i < 100; i++) {
 		sample1 = sample1 + analogRead((uint8_t)BAT_PIN); //read the voltage from the divider circuit
 		delay(2);
@@ -184,7 +205,7 @@ void loop() {
 	sample1 = sample1 / 100;
 	batVolt = (sample1
 			/ (1023.0
-					- ((batResValueGnd / (batResValueVolt + batResValueGnd))
+					- ((BAT_RES_VALUE_GND / (BAT_RES_VALUE_VCC + BAT_RES_VALUE_GND))
 							* 1023.0))) * vcc;
 
 	// *********  Measuring Resistor Voltage ***********
@@ -195,7 +216,7 @@ void loop() {
 	sample2 = sample2 / 100;
 	resVolt = (sample2
 			/ (1023.0
-					- ((resResValueGnd / (resResValueVolt + resResValueGnd))
+					- ((RES_RES_VALUE_GND / (RES_RES_VALUE_VCC + RES_RES_VALUE_GND))
 							* 1023.0))) * vcc;
 
 	realBatVolt = (mosfetStatus == HIGH) ? batVolt + resVolt : batVolt;
@@ -222,44 +243,80 @@ void loop() {
 		if (buttonSelectState==HIGH) {
 			batterySelected = tmpBatterySelected;
 			batHigh = batteryTipes[tmpBatterySelected].maxVolt; // Battery High Voltage
-			batLow = batteryTipes[tmpBatterySelected].minVolt; // Discharge Cut Off Voltage
+			batLow = batteryTipes[tmpBatterySelected].minVolt - minVoltageDropoutTolerance; // Discharge Cut Off Voltage
+//			startingVcc = batHigh;
+			// I put batt low value so I show 0% until the correct voltage is grabbed
+			startingVcc = batLow;
 		}
 		delay(10);
+
+		// Reset millis
+		previousMillis = millis();
 	// Temperature warning
-	}else if (batteryTemp > BATTERY_MAX_TEMP || resistanceTemp > RESISTANCE_MAX_TEMP) {
+	}else if ((USING_BATTERY_TERMISTOR && batteryTemp > BATTERY_MAX_TEMP) || (USING_RESISTO_TERMISTOR && resistanceTemp > RESISTANCE_MAX_TEMP)) {
 		digitalWrite(MOSFET_PIN, LOW); // Turned Off the MOSFET // No discharge
 		mosfetStatus = LOW;
 		beep(100);
+		delay(2000);
+
 		// Reset millis
 		previousMillis = millis();
-		delay(1000);
 	// Voltage too hight
 	} else if (realBatVolt > batHigh) {
 		digitalWrite(MOSFET_PIN, LOW); // Turned Off the MOSFET // No discharge
 		mosfetStatus = LOW;
 		beep(200);
 		delay(1000);
-	}
-	// Voltage too low
-	else if (realBatVolt < batLow) {
+
+		// Reset millis
+		previousMillis = millis();
+	// No battery
+	} else if (realBatVolt < NO_BATTERY_VOLTAGE) {
 		digitalWrite(MOSFET_PIN, LOW);
 		mosfetStatus = LOW;
 		beep(200);
 		delay(1000);
+
+		// Reset millis
+		previousMillis = millis();
+	// Voltage too low
+	} else if (realBatVolt < batLow || lowRaised>NUMBER_OF_LOW_TO_RAISE) {
+		// Now discharging is considered started when al least wone time is calculated maha
+		// So need to check mosfet status if some people attach discharged battery to device
+		if (dischargingStarted || mosfetStatus==HIGH) {
+			dischargingStarted = true;
+			if (lowRaised<=NUMBER_OF_LOW_TO_RAISE) lowRaised++; // To prevent overflow
+		}
+
+		digitalWrite(MOSFET_PIN, LOW);
+		mosfetStatus = LOW;
+		beep(200);
+		delay(WAIT_AFTER_LOW);
 	// Discharging ok
 	} else if (realBatVolt > batLow && realBatVolt < batHigh
 			&& (previousMillis == 0
 					|| (millis() - previousMillis) > waitMillisPassed)) { // Check if the battery voltage is within the safe limit
-		digitalWrite(MOSFET_PIN, HIGH);
-		mosfetStatus = HIGH;
-		millisPassed = millis() - previousMillis;
-		current = (batVolt - resVolt) / resValue;
-		mA = current * 1000.0;
-		capacity = capacity + mA * (millisPassed / 3600000.0); // 1 Hour = 3600000ms
-		previousMillis = millis();
-		row++;
-		x++;
+		// startingVcc used to get percentage of discharging
+		if (!dischargingStarted && mosfetStatus == HIGH) {
+			startingVcc = realBatVolt;
+			dischargingStarted = true;
+		}
 
+		// When battery is under discharging change his voltage and now I reset previous millis to have a resonable value
+		if (mosfetStatus==LOW){
+			digitalWrite(MOSFET_PIN, HIGH);
+			mosfetStatus = HIGH;
+			delay(2000);
+			previousMillis = millis();
+		}else{
+			millisPassed = millis() - previousMillis;
+			current = (batVolt - resVolt) / resValue;
+			mA = current * 1000.0;
+			capacity = capacity + mA * (millisPassed / 3600000.0); // 1 Hour = 3600000ms
+			previousMillis = millis();
+			row++;
+			x++;
+		}
 		delay(1000);
 	}
 
@@ -283,6 +340,7 @@ void draw(void) {
 			lcd.clear();
 
 			lcd.setCursor(0, 0);
+			lcd.print("B Tp: ");
 			lcd.print(batteryTipes[tmpBatterySelected].name);
 			lcd.setCursor(0, 1);
 			lcd.print("Volt: ");
@@ -325,7 +383,7 @@ void draw(void) {
 			lcd.print("C");
 		}
 	// No battery
-	} else if (realBatVolt < 1) {
+	} else if (realBatVolt < NO_BATTERY_VOLTAGE) {
 		if (dischargingStarted /*prevStatus==4*/) {
 			lcd.setCursor(0, 0);
 			lcd.write(byte(loadLenght + 0));
@@ -371,42 +429,54 @@ void draw(void) {
 		}
 		prevStatus = 2;
 	// Battery is discharged
-	} else if (realBatVolt < batLow) {
+	} else if (realBatVolt < batLow  || lowRaised>NUMBER_OF_LOW_TO_RAISE) {
 		if (dischargingStarted /*prevStatus==4*/) {
 			lcd.setCursor(0, 0);
 			lcd.write(byte(loadLenght + 1));
 
 			lcd.setCursor(2, 0);
 			lcd.print("Low ");
+
+			// Need to display voltege battery when It's start discharged
+			if (prevStatus != 3){
+				lcd.setCursor(0, 1);
+				lcd.print(realBatVolt, 2);  // display Battery Voltage in Volt
+				lcd.print("V");
+			}
 		} else if (prevStatus != 3) {
 			lcd.setCursor(0, 0);
 			lcd.print("Low-V!");
 
-			Serial.print("Low-V!");
-
-			Serial.print(" - ");
-
-			Serial.print("Volt: ");
-			Serial.print(realBatVolt, 2);  // display Battery Voltage in Volt
-			Serial.print("V");
-
-			Serial.print(" - ");
-
-			Serial.print("DISCHARGING: ");
-			Serial.println(mosfetStatus);     // display capacity in mAh
-
+			lcd.setCursor(0, 1);
+			lcd.print(realBatVolt, 2);  // display Battery Voltage in Volt
+			lcd.print("V");
 		}
+
+
+
+		Serial.print("Low-V!");
+
+		Serial.print(" - ");
+
+		Serial.print("Volt: ");
+		Serial.print(realBatVolt, 2);  // display Battery Voltage in Volt
+		Serial.print("V");
+
+		Serial.print(" - ");
+		Serial.print("lowRaised");
+		Serial.print(lowRaised);  // display Battery Voltage in Volt
+
+		Serial.print(" - ");
+
+		Serial.print("DISCHARGING: ");
+		Serial.println(mosfetStatus);     // display capacity in mAh
+
 		prevStatus = 3;
 
 	// Discharging time
 	} else if (realBatVolt >= batLow && realBatVolt < batHigh) {
-		// startingVcc used to get percentage of discharging
-		if (!dischargingStarted) {
-			startingVcc = realBatVolt;
-		}
-		dischargingStarted = true;
 		// If prevState is low voltage and voltage less the tolerance not greater than battery low voltage cycle is finished
-		if (!(prevStatus == 3 && ((realBatVolt - tolerance) < batLow))) {
+//		if (!(prevStatus == 3 && ((realBatVolt - tolerance) < batLow))) {
 			lcd.clear();
 
 			lcd.setCursor(0, 0);
@@ -440,16 +510,16 @@ void draw(void) {
 			lcd.print(mA, 0);  // display current in mA
 			lcd.print("mA");
 
-			if (lastTempVisualizedBattery < 3) {
+			if ((USING_BATTERY_TERMISTOR && lastTempVisualizedBattery < 3)) {
 				lcd.print(" B");
 
-				lcd.print(batteryTemp, 0);  // display current in mA
+				lcd.print(batteryTemp, 0);  // Battery Temp in °C
 				lcd.write(byte(loadLenght + 3));
 
-			} else if (lastTempVisualizedBattery < 7) {
+			} else if ((USING_RESISTO_TERMISTOR && lastTempVisualizedBattery < 7)) {
 				lcd.print(" R");
 
-				lcd.print(resistanceTemp, 0);  // display current in mA
+				lcd.print(resistanceTemp, 0);  // Resitor Temp in °C
 				lcd.write(byte(loadLenght + 3));
 			}
 			lastTempVisualizedBattery++;
@@ -458,7 +528,7 @@ void draw(void) {
 			}
 
 			Serial.print("Volt: ");
-			Serial.print(realBatVolt, 2);  // display Battery Voltage in Volt
+			Serial.print(realBatVolt, 2);  // display Battery Voltage + res voltage in Volt
 			Serial.print("V");
 
 			Serial.print(" - ");
@@ -469,14 +539,8 @@ void draw(void) {
 
 			Serial.print(" - ");
 
-			Serial.print("M Volt: ");
-			Serial.print(((batVolt + realBatVolt) / 2), 2); // display Battery Voltage in Volt
-			Serial.print("V");
-
-			Serial.print(" - ");
-
 			Serial.print("Res Volt: ");
-			Serial.print(resVolt, 2);  // display Battery Voltage in Volt
+			Serial.print(resVolt, 2);  // display Resistor Voltage in Volt
 			Serial.print("V");
 
 			Serial.print(" - ");
@@ -493,18 +557,18 @@ void draw(void) {
 			Serial.print(" - ");
 
 			Serial.print("DISCHARGING: ");
-			Serial.print(mosfetStatus);     // display capacity in mAh
+			Serial.print(mosfetStatus);     // display mosfet status
 
 			Serial.print(" - ");
 
 			Serial.print("Battery temp: ");
-			Serial.print(batteryTemp, 2);     // display capacity in mAh
+			Serial.print(batteryTemp, 2);     // display battery temp
 
 			Serial.print(" - ");
 
 			Serial.print("Resistance temp: ");
-			Serial.println(resistanceTemp, 2);     // display capacity in mAh
-		}
+			Serial.println(resistanceTemp, 2);     // display resistance temp
+//		}
 		prevStatus = 4;
 	}
 }
--- README.md
+++ README.md
@@ -1,16 +1,52 @@
-# Battery Capacity Tester Arduino NANO #
-Another Battery capacity tester.
+# Battery Capacity Tester Arduino NANO compatible #
+Battery capacity tester.
 With this device you can check the 18650 battery capacity. For safe I added with thermistor the temperature of power resistance and battery to prevent explosion.
-I'm doing some change from original project and now It's quite fine:
+I'm doing some change from original project:
 v0.1
  - VCC of Arduino now It's automatically calculated;
- - Voltage on screen now It's correct; 
  - Added variable to change setting in more confortable way.
  - Added percentage of discharging
  - Added temperature of battery and power resistor
  
 v0.2
  - Added possiblity of battery selection
+ - Created prototype board (Look the schema), screen, button and speaker is out of board because in the future I'd like to create a package. 
+
+In v0.2 to support various type of battery I created a struct that must be filled with battery name, min voltage, e max voltage *(I need help to fill :P)*.
+```cpp
+// Structure of battery type
+struct BatteryType {
+	char name[10];
+	float maxVolt;
+	float minVolt;
+};
+
+#define BATTERY_TYPE_NUMBER 4
+BatteryType batteryTipes[BATTERY_TYPE_NUMBER] = {
+		{ "18650", 4.3, 2.9 },
+		{ "17550", 4.3, 2.9 },
+		{ "14500", 4.3, 2.75 },
+		{ "6v Acid", 7.2, 6.44  }
+};
+```
+Now I use 10k resistor for voltage read, if you want change voltage support you must change this value *(explain better next)*:
+```cpp
+// Battery voltage resistance
+#define BAT_RES_VALUE_GND 10.0
+#define BAT_RES_VALUE_VCC 10.0
+// Power resistor voltage resistance
+#define RES_RES_VALUE_GND 10.0
+#define RES_RES_VALUE_VCC 10.0
+```
+If you don't use thermistor set to false this:
+```cpp
+#define USING_BATTERY_TERMISTOR true
+#define USING_RESISTO_TERMISTOR true
+```
+If you use a different i2c display you must rewrite this method:
+```cpp
+void draw(void);
+```
 
 In project you can find fritzing, schema, photo and other.
 
@@ -19,10 +55,92 @@ In project you can find fritzing, schema, photo and other.
 
 Created from an idea of [OpenGreenEnergy](http://www.instructables.com/id/DIY-Arduino-Battery-Capacity-Tester-V10-/).
 
-## Wire schema ##
+## Realization ##
+To measuring voltage we use the principle of Voltage divider, more information on:
+[Wikipedia](https://en.wikipedia.org/wiki/Voltage_divider) or [Blog](https://startingelectronics.org/articles/arduino/measuring-voltage-with-arduino/), in simple word this code 
+```cpp
+batVolt = (sample1 / (1023.0 - ((batResValueGnd / (batResValueVolt + batResValueGnd)) * 1023.0))) * vcc;
+```
+measure battery voltage,
+`batResValueGnd / (batResValueVolt + batResValueGnd)` this is the multipler factor of the reading voltage, I inserted 2 resistance of `batResValueVolt` and `batResValueGnd` ohom after and before the analog read wire;
+`sample1 -->` is the average analog readings;
+`vcc -->` reference arduino voltage;
+`1023.0 -->` is the reference maximun value of analog read (arduino analog read go from 0 to 1023)
+	
+The principle is that we measure the voltage afther and before the power resistor and so we can calculate milliampere that consumes the battery.
 
-![Breadboard schema](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/BTesterV02_bb.png)
-![Schema](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/BTester_schem.png)
+The MOSFET is used to stard and stop battery drain from power resistor.
+I'm pretty scary, so I inserted 2 thermistors to monitorage battery and power resistor temperature.
+
+### Versione v0.2 ###
+---
+
+**On breadboard:**
+
+![Breadboard](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/BTesterV02_bb.jpg)
+
+![Battery selection](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/BB_Select_battery.jpg)
+
+**Serial output for debug:**
+
+```
+Volt: 2.94V - B Volt: 2.94V - Res Volt: 0.00V - Curr: 292mA - mAh: 219.4 - DISCHARGING: 1 - Battery temp: 19.47 - Resistance temp: 32.28
+Volt: 2.94V - B Volt: 2.94V - Res Volt: 0.00V - Curr: 292mA - mAh: 219.4 - DISCHARGING: 1 - Battery temp: 19.47 - Resistance temp: 32.24
+Volt: 2.94V - B Volt: 2.94V - Res Volt: 0.00V - Curr: 292mA - mAh: 219.4 - DISCHARGING: 1 - Battery temp: 19.44 - Resistance temp: 32.28
+Volt: 2.92V - B Volt: 2.92V - Res Volt: 0.00V - Curr: 292mA - mAh: 219.7 - DISCHARGING: 1 - Battery temp: 19.49 - Resistance temp: 32.28
+Volt: 2.94V - B Volt: 2.94V - Res Volt: 0.00V - Curr: 292mA - mAh: 219.7 - DISCHARGING: 1 - Battery temp: 19.46 - Resistance temp: 32.26
+Volt: 2.94V - B Volt: 2.94V - Res Volt: 0.00V - Curr: 292mA - mAh: 219.7 - DISCHARGING: 1 - Battery temp: 19.42 - Resistance temp: 32.28
+Volt: 2.92V - B Volt: 2.92V - Res Volt: 0.00V - Curr: 292mA - mAh: 219.7 - DISCHARGING: 1 - Battery temp: 19.46 - Resistance temp: 32.22
+Volt: 2.94V - B Volt: 2.94V - Res Volt: 0.00V - Curr: 294mA - mAh: 220.0 - DISCHARGING: 1 - Battery temp: 19.46 - Resistance temp: 32.26
+```
+
+**On prototype board:**
+I create a prototype board very extendible, but for now I use only minimal set of pin (in future led and other button).
+If you want support voltage greater than 10v you must change the resistor value of Battery and Resistance in according to the formula
+`(BAT_RES_VALUE_GND / (BAT_RES_VALUE_VCC + BAT_RES_VALUE_GND)` in the schema `Resistor power voltage GND 1/2 / (Resistor power voltage 2/2 + Resistor power voltage GND 1/2)`.
+
+![Prototype](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PTesterV02_bb.jpg)
+
+![Prototype schema](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PTesterV02_schem.jpg)
+
+**Shopping List**
+
+| Amount | Part Type | Properties |
+| --- | --- | --- |
+| 2 |  5mm Screw TermInal [eBay](https://www.ebay.com/itm/20pcs-2-Pole-5mm-Pitch-PCB-Mount-Screw-TermInal-Block-8A-250V-LW-SZUS/181932294528?epid=1553081429&hash=item2a5c028180:g:SUIAAOSwZjhXIvSX) | pin spacing 0.2in (5.08mm);  2 Pole 5mm Pitch PCB Mount Screw TermInal Block 8A 250V LW SZUS |
+| 1 | Arduino Pro Mini clone (compatible Nano) [eBay](https://www.ebay.com/itm/New-design-Pro-Mini-atmega328-5V-16M-Replace-ATmega128-Arduino-Compatible-Nano/171558370113?hash=item27f1acfb41:g:CjwAAOSwQJhUdgXb) | tipo Arduino Pro Mini (Clone comp Nano); variant variant 1 |
+| 1 | Basic FET P-Channel **IRF744N or IRLZ44N** [eBay](https://www.ebay.com/itm/5PCS-IRLZ44N-TO-220-IRLZ44-TO220-IRLZ44NPBF-Effect-Transistor/192345139104?hash=item2cc8a9e7a0:g:Ca4AAOSwqbxZ8cgT) | package TO220 [THT]; tipo p-channel |
+| 11 | 10kΩ Resistor | bands 4; pin spacing 400 mil; tolerance ±5%; package THT; resistenza 10kΩ |
+| 1 | Temperature Sensor (Thermistor) [eBay](https://www.ebay.com/itm/10Pcs-Temperature-Sensor-Ntc-Mf58-3950-B-10K-Ohm-5-Thermistor-New-Ic-T/232317930330?epid=1048450678&hash=item36173a4b5a:g:W5wAAOSwcLxYHcpf) | package THT; thermistor type NTC; resistance at 25° **10kΩ**; tipo thermistor |
+| * | Generic male header | form ♂ (male); pin spacing 0.1in (2.54mm); package THT; row single; hole size 1.0mm,0.508mm; pins 2 |
+| * | Generic female header | form ♀ (female); pin spacing 0.1in (2.54mm); package THT; row single; hole size 1.0mm,0.508mm; pins 2 |
+| PerfBoard board | Prototype board [eBay](https://www.ebay.com/itm/Novelty-Universal-Bakelite-Circuit-Board-DIY-Prototype-PCB-Rectangle-Device-5pcs/142528992310?epid=1263113777&hash=item212f63d436:g:2kUAAOSwT6pVg3~e) | 24x18  |
+
+[Complete list of all part](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PTesterV02_bom.html)
+
+Soldering prototype:
+
+![Pin up](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PB_01_pin_up.jpg)
+![Pin bottom](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PB_01_pin_bottom.jpg)
+![Pull up](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PB_03_pulldown_up.jpg)
+![pull down](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PB_05_pulldown_connect_bottom.jpg)
+![All up](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PB_06_allother_up.jpg)
+![All bottom](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PB_07_allother_bottom.jpg)
+![PB_TestMAHA](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/PB_TestMAHA.jpg)
+
+**Display**
+
+![Sel battery](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/Disp_sel_battery.jpg)
+![Battery removed](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/Disp_battery_removed.jpg)
+![Resistance hot](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v02/Disp_Resistance_hot.jpg)
+
+### Version v0.1 ###
+---
+
+## Wire schema v0.1 ##
+
+![Breadboard schema](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v01/BTesterV02_bb.png)
+![Schema](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v01/BTester_schem.png)
 
 ## Assembly list ##
 | Label | Part Type | Properties |
@@ -38,7 +156,7 @@ Created from an idea of [OpenGreenEnergy](http://www.instructables.com/id/DIY-Ar
 | IRF744N | Basic FET N-Channel | tipo n-channel; package TO220 [THT] |
 | MF58 | Temperature Sensor (Thermistor) | resistance at 25° 10kΩ; tipo thermistor; package THT; thermistor type NTC |
 | MF58 | Temperature Sensor (Thermistor) | resistance at 25° 10kΩ; tipo thermistor; package THT; thermistor type NTC |
-| Microcontroller | Arduino Pro Mini clone (compatible Nano) | variant variant 1; tipo Arduino Pro Mini (Clone comp Nano) |
+| Microcontroller | Arduino Pro Mini clone (compatible Nano) [eBay](https://www.ebay.com/itm/New-design-Pro-Mini-atmega328-5V-16M-Replace-ATmega128-Arduino-Compatible-Nano/171558370113?hash=item27f1acfb41:g:CjwAAOSwQJhUdgXb) | variant variant 1; tipo Arduino Pro Mini (Clone comp Nano) |
 | Power resistor | 10kΩ Resistor | pin spacing 400 mil; bands 5; resistenza 10kΩ; tolerance ±5%; package THT; part number Power resistor |
 | Resistance of battery volt. 1/2 | 10kΩ Resistor | pin spacing 400 mil; bands 4; resistenza 10kΩ; tolerance ±5%; package THT |
 | Resistance of battery volt. 2/2 | 10kΩ Resistor | pin spacing 400 mil; bands 4; resistenza 10kΩ; tolerance ±5%; package THT |
@@ -66,31 +184,15 @@ Created from an idea of [OpenGreenEnergy](http://www.instructables.com/id/DIY-Ar
 
 *\* Only if you want assembly LCD i2c character controller*
 
-## Realization ##
-To measuring voltage we use the principle of Voltage divider, more information on:
-[Wikipedia](https://en.wikipedia.org/wiki/Voltage_divider) or [Blog](https://startingelectronics.org/articles/arduino/measuring-voltage-with-arduino/), in simple word this code 
-```cpp
-batVolt = (sample1 / (1023.0 - ((batResValueGnd / (batResValueVolt + batResValueGnd)) * 1023.0))) * vcc;
-```
-measure battery voltage,
-`batResValueGnd / (batResValueVolt + batResValueGnd)` this is the multipler factor of the reading voltage, I inserted 2 resistance of `batResValueVolt` and `batResValueGnd` ohom after and before the analog read wire;
-`sample1 -->` is the average analog readings;
-`vcc -->` reference arduino voltage;
-`1023.0 -->` is the reference maximun value of analog read (arduino analog read go from 0 to 1023)
-	
-The principle is that we measure the voltage afther and before the power resistor and so we can calculate milliampere that consumes the battery.
+**Breadboard**
 
-The MOSFET is used to stard and stop battery drain from power resistor.
-I'm pretty scary, so I inserted 2 thermistors to monitorage battery and power resistor temperature.
-
-![Breadboard](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/breadboard01.jpg)
-![lcd on discharging](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/lcdDischarging02.jpg)
+![Breadboard](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v01/breadboard01.jpg)
+![lcd on discharging](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v01/lcdDischarging02.jpg)
 
 Thermistor on power resistor (heat shrink sleeve to hold).
 
-![Thermistor on power resistance](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/thermistorPowerResistance.jpg)
+![Thermistor on power resistance](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v01/thermistorPowerResistance.jpg)
 
 Thermistor on battery.
 
-![Thermistor on battery](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/thermistorBattery.jpg)
-
+![Thermistor on battery](https://github.com/xreef/BatteryCapacityTesterArduinoNANO/blob/master/Resources/v01/thermistorBattery.jpg)
